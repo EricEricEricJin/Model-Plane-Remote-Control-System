baadb3285a81c720cc73dee737f84e6bec7eb1de
ericericericjin <jyseric@126.com>
2020-04-24 08:45:11 +0800

    apr.24

--- FMC/engine_test.py
+++ FMC/engine_test.py
@@ -3,8 +3,11 @@ if __name__ == "__main__":
     E = Engine()
     E.init()
     while True:
-        pwr = int(input("pwr: "))
-        if pwr == 114514:
-            break
-        E.change_pwr(pwr)
+        try:
+            pwr = eval(input("pwr: "))
+            if pwr == 114514:
+                break
+            E.change_pwr(pwr)
+        except:
+            print("?")
     del(E)
